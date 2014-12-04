5e647e93f8a2aa03634614984fe1d132cf7bbf99
Robertslob <rjrslob@gmail.com>
2014-12-04 13:29:50 +0100

    Updated factorial with long and added nCr and nPr

--- MathFunctions/ProbabilityFunctions.java
+++ MathFunctions/ProbabilityFunctions.java
@@ -2,11 +2,30 @@ package com.teamindecisive.calcurmath;
 
 public class ProbabilityFunctions 
 {
-	public static int factorial(int a)
+	public static long factorial(int a)
 	{
-		int b = 1;
-		for(int n = a; n > 0; n--)
+		long b = 1;
+		for(int n = 1; n <= a; n++)
 			b*=n;
 		return b;
 	}
+	
+	// nPr method
+	public static long permutations(int a, int b)
+	{
+		// The formula is: a!/(a-b)!
+		// Which equals the following for-loop
+		long c = 1;
+		for(int n = a; n>(a-b); n--)
+			c*=n;
+		return c;
+	}	
+		
+	// nCr method
+	public static long combinations(int a, int b)
+	{
+		// The formula is: a!/(b!*(a-b)!))
+		long r = factorial(b);
+		return permutations(a,b)/r;
+	}	
 }
