2e61b77713dc496fa0f1ffb78ef930dd847b1e80
Robertslob <rjrslob@gmail.com>
2014-12-26 13:25:35 +0100

    added an approximation and updated the summation such that it can give an approximation for the summation aswell

--- MathFunctions/function/Calculus.java
+++ MathFunctions/function/Calculus.java
@@ -1,9 +1,10 @@
 package com.teamindecisive.calcurmath.function;
 
-import org.matheclipse.core.basic.Config;
+import org.matheclipse.core.eval.EvalDouble;
 import org.matheclipse.core.eval.EvalUtilities;
 
 // A string like the following should be given with "diff(sin(x)*cos(x),x)" or "integrate(sin(x)^5,x)"
+// Other expressions can be given aswell, like "1/2+1/3" which returns "5/6"
 // There will be a string returned like "-Sin(x)^2+Cos(x)^2" (note the capitals)
 // For more details, see https://bitbucket.org/axelclk/symja_android_library/wiki/examples/Calculus
 public class Calculus 
@@ -11,20 +12,31 @@ public class Calculus
 	// The string must be written with lowerCases
 	public static String lowerCase(String s)
 	{
-		EvalUtilities util = new EvalUtilities(true, false);
+		EvalUtilities util = new EvalUtilities(false, true);
 		return util.evaluate(s).toString();
 	}
-	/**
-	// Hoe we dit implementeren is afhankelijk van Daan, kan nu nog niet heel veel mee
-	// startwaarde a t/m b
-	public static String summation(String s, int a, int b)
+	
+	public static String doubleLowerCase(String s)
 	{
-		// declareer hier een waarde ding
-		for (int n = a; n<=b; n++)
-			c = n;
-			// Waarde ding += Functiewaarde(s, n);
-			// Kan hier nog niks doen zonder Daan
-		// return waarde ding
+		EvalDouble doubleUtil = new EvalDouble(true);
+		return String.valueOf(doubleUtil.evaluate(s));		
 	}
-	**/
+	
+	// A summation from a to b with function s
+	// If the boolean is true, the answer is exact, otherwise its an approximation
+	// example of s: "Sin(x)" 
+	public static String summation(String s, int a, int b, boolean c)
+	{
+		String result = "";
+		for (int n = a; n<=b; n++)
+		{
+			lowerCase("x =" + n);
+			result += lowerCase(s);
+			if (!(b==n))
+				result += "+";
+		}
+		if(c)
+			return lowerCase(result);
+		else return doubleLowerCase(result);
+	}
 }
