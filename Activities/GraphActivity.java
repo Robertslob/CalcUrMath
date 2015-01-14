98ed1880240fcb7e364025763908d431fbbd3ad7
Robertslob <rjrslob@gmail.com>
2015-01-14 01:14:58 +0100

    Put graphsettings into graphactivity

--- Activities/GraphActivity.java
+++ Activities/GraphActivity.java
@@ -1,56 +1,44 @@
 package com.teamindecisive.calcurmath;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
-public class GraphActivity extends ActionBarActivity 
+import com.teamindecisive.calcurmath.function.Calculus;
+
+public class GraphActivity extends ActionBarActivity
 {	
-	double xMin;
-	double xMax;
-	int xScl;
-	int yScl;
-	
+	public GraphView graph;
+	public double xMin = -10.0;
+	double xMax = 10.0;
+	int xScl = 10;
+	int yScl = 10;
+	String[] functions = new String[6];
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
-		getAxes();
-		getFunctions();
-        float[] xvalues = new float[(int)(xMax-xMin)*100+1];
-        float[] yvalues = new float[(int)(xMax-xMin)*100+1];
-	    for (int i=0;i<(int)(xMax-xMin)*100+1;i++)
-		{        	
-			double temp = (xMin+i*.01);	
-        	xvalues[i] = (float)temp;	
-        	yvalues[i] = (float)(10*Math.sin(temp));
-	    }     
-	    GraphView graph = new GraphView(this, xvalues, yvalues, 1);
-	    setContentView(graph);
+		setContentView(R.layout.activity_settings);
+		Button btn = (Button) findViewById(R.id.graphButton);
+		btn.setTransformationMethod(null);
+		btn.setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View v){
+				getAxesFromText();
+				getFunctionsFromText();
+				buildStuff();
+			}
+		});
+		/*getAxesFromText();
+		getFunctionsFromText();
+		buildStuff();*/
 	}
 	
-	protected void getAxes()
-	{
-		EditText xMinEdit = (EditText) findViewById(R.id.xMin_Edit);
-		EditText xMaxEdit = (EditText) findViewById(R.id.xMax_Edit);
-		EditText xSclEdit = (EditText) findViewById(R.id.xScl_Edit);
-		EditText ySclEdit = (EditText) findViewById(R.id.yScl_Edit);
-		// nog Try-catch van maken
-		xMin = Double.parseDouble(xMinEdit.getText().toString());
-		xMax = Double.parseDouble(xMaxEdit.getText().toString());
-		xScl = Integer.parseInt(xSclEdit.getText().toString());
-		yScl = Integer.parseInt(ySclEdit.getText().toString());
-	}
-	
-	protected void getFunctions()
-	{
-		// Array?
-	}
-
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) 
 	{
@@ -71,8 +59,59 @@ public class GraphActivity extends ActionBarActivity
 	
 	public void openSettings()
 	{
-		Intent startActivity = new Intent(this, GraphSettings.class);
-	    startActivity(startActivity);
+		setContentView(R.layout.activity_settings);
+		Button btn = (Button) findViewById(R.id.graphButton);
+		btn.setTransformationMethod(null);
+		btn.setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View v){
+				getAxesFromText();
+				getFunctionsFromText();
+				buildStuff();
+			}
+		});
+	}
+	
+	protected void getAxesFromText(){
+		EditText xMinEdit = (EditText) findViewById(R.id.xMin_Edit);
+		EditText xMaxEdit = (EditText) findViewById(R.id.xMax_Edit);
+		EditText xSclEdit = (EditText) findViewById(R.id.xScl_Edit);
+		EditText ySclEdit = (EditText) findViewById(R.id.yScl_Edit);
+		// nog Try-catch van maken
+		xMin = Double.parseDouble(xMinEdit.getText().toString());
+		xMax = Double.parseDouble(xMaxEdit.getText().toString());
+		xScl = Integer.parseInt(xSclEdit.getText().toString());
+		yScl = Integer.parseInt(ySclEdit.getText().toString());
+	}
+	
+	protected void getFunctionsFromText(){
+		EditText [] editFunctions = new EditText[6];
+		editFunctions[0] = (EditText) findViewById(R.id.F1_Edit);
+		editFunctions[1] = (EditText) findViewById(R.id.F2_Edit);
+		editFunctions[2] = (EditText) findViewById(R.id.F3_Edit);
+		editFunctions[3] = (EditText) findViewById(R.id.F4_Edit);
+		editFunctions[4] = (EditText) findViewById(R.id.F5_Edit);
+		editFunctions[5] = (EditText) findViewById(R.id.F6_Edit);		
+		for(int i=0; i<6; i++)
+			if(!(editFunctions[i].getText().toString().matches("") | editFunctions[i].getText().toString() == null))
+				functions[i] = editFunctions[i].getText().toString();
+	}
+	
+	protected void buildStuff()
+	{
+		float[] xvalues = new float[(int)(xMax-xMin)*100+1];
+        float[][] functionValues = new float[6][(int)(xMax-xMin)*100+1];
+	    for (int i=0;i<(int)(xMax-xMin)*100+1;i++)
+		{        	
+			double temp = (xMin+i*.01);	
+        	xvalues[i] = (float)temp;
+        	String stringTemp = String.valueOf(temp);
+        	for(int n = 0; n<6; n++)
+        		if(!(functions[n] =="" | functions[n]==null))
+        			functionValues[n][i] = (float)Double.parseDouble(Calculus.getValue(functions[n], stringTemp, false));       
+		}
+	    graph = new GraphView(this, xvalues, functionValues, xScl, yScl);
+	    setContentView(graph);
 	}
 }
 
